96c0b4d017c251b7e4e99783310cf888b9f37fe2
Stanislav Kozlov <bleymy@mail.ru>
2019-07-27 17:01:39 +0300

    Make main fuctionality

--- Quizzzz/Controller/ViewController.swift
+++ Quizzzz/Controller/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    let questionBank = QuestionBank()
+    
+    var pickedAnswer : Bool = false
+    var currentQuestionIndex : Int = 0
+    var scorePoints : Int = 0
+    var questionProgressCounter : Int = 0
+    var numberOfQuestions : Int = 0
+    
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,32 +30,63 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        numberOfQuestions = questionBank.list.count
+        updateUI()
     }
     
     
-    @IBAction func clickTrue(_ sender: UIButton) {
+    @IBAction func answerPressed(_ sender: UIButton) {
+        pickedAnswer = (sender.tag == 1) ? true : false
+        checkAnswer()
+        
     }
     
-    @IBAction func clickFalse(_ sender: UIButton) {
-    }
-    
+    //update ui
     func updateUI(){
+        questionLabel.text = questionBank.list[currentQuestionIndex].questionText
+        scoreLabel.text = "\(scorePoints)"
+        progressLabel.text = "\(currentQuestionIndex + 1)/\(numberOfQuestions)"
         
     }
-    
+    //go to next question
     func nextQuestion(){
+        currentQuestionIndex += 1
+        if currentQuestionIndex < questionBank.list.count {
+            updateUI()
+        } else {
+            //create alert controller
+            let alert = UIAlertController(title: "Your score is \(scorePoints)",
+                                          message: "Do you want to start again?",
+                                          preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: NSLocalizedString("Restart", comment: "Default action"),
+                                              style: .default,
+                                              handler:{ (_) in
+                                                self.startOver()
+            })
+            alert.addAction(restartAction)
+            self.present(alert, animated: true, completion: nil)
+        }
         
     }
-    
+    //start quizz again
     func startOver(){
-        
+        currentQuestionIndex = 0
+        scorePoints = 0
+        updateUI()
     }
-    
+    //check the answer
     func checkAnswer(){
+        if pickedAnswer == questionBank.list[currentQuestionIndex].answer {
+            scorePoints += 1
+            print("correct")
+        } else {
+            print("incorrect")
+        }
         
+        nextQuestion()
     }
     
-
-
+    
+    
 }
 
